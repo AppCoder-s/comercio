7002a6ca89ea84167e249a032d82aa6e30162ad5
rodrigo camargo gonzalez <josue_1321124111@uptecamac.edu.mx>
2023-09-27 13:21:34 -0600

    Rutas josue

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,191 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('404');
+});
+
+Route::get('/', function () {
+    return view('about-us');
+});
+
+Route::get('/', function () {
+    return view('blog-2-column');
+});
+
+Route::get('/', function () {
+    return view('blog-3-column');
+});
+
+Route::get('/', function () {
+    return view('blog-audio-format');
+});
+
+Route::get('/', function () {
+    return view('blog-details-left-sidebar');
+});
+
+Route::get('/', function () {
+    return view('blog-details-right-sidebar');
+});
+
+Route::get('/', function () {
+    return view('blog-details');
+});
+
+Route::get('/', function () {
+    return view('blog-gallery-format');
+});
+
+Route::get('/', function () {
+    return view('blog-left-sidebar');
+});
+
+Route::get('/', function () {
+    return view('blog-list-left-sidebar');
+});
+
+Route::get('/', function () {
+    return view('blog-list-right-sidebar');
+});
+
+Route::get('/', function () {
+    return view('blog-list');
+});
+
+Route::get('/', function () {
+    return view('blog-right-sidebar');
+});
+
+Route::get('/', function () {
+    return view('blog-video-format');
+});
+
+Route::get('/', function () {
+    return view('cart');
+});
+
+Route::get('/', function () {
+    return view('checkout');
+});
+
+Route::get('/', function () {
+    return view('compare');
+});
+
+Route::get('/', function () {
+    return view('contact');
+});
+
+Route::get('/', function () {
+    return view('faq');
+});
+
+Route::get('/', function () {
+    return view('index-2');
+});
+
+Route::get('/', function () {
+    return view('index-3');
+});
+
+Route::get('/', function () {
+    return view('index-4');
+});
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/', function () {
+    return view('login-register');
+});
+
+Route::get('/', function () {
+    return view('product-details');
+});
+
+Route::get('/', function () {
+    return view('shop-3-column');
+});
+
+Route::get('/', function () {
+    return view('shop-4-column');
+});
+
+Route::get('/', function () {
+    return view('shop-left-sidebar');
+});
+
+Route::get('/', function () {
+    return view('shop-list-left-sidebar');
+});
+
+Route::get('/', function () {
+    return view('shop-list-right-sidebar');
+});
+
+Route::get('/', function () {
+    return view('shop-list');
+});
+
+Route::get('/', function () {
+    return view('shop-right-sidebar');
+});
+
+Route::get('/', function () {
+    return view('shopping-cart');
+});
+
+Route::get('/', function () {
+    return view('single-product-affiliate');
+});
+
+Route::get('/', function () {
+    return view('single-product-carousel');
+});
+
+Route::get('/', function () {
+    return view('single-product-gallery-left');
+});
+
+Route::get('/', function () {
+    return view('single-product-gallery-right');
+});
+
+Route::get('/', function () {
+    return view('single-product-group');
+});
+
+Route::get('/', function () {
+    return view('single-product-normal');
+});
+
+Route::get('/', function () {
+    return view('single-product-sale');
+});
+
+Route::get('/', function () {
+    return view('single-product-tab-style-left');
+});
+
+Route::get('/', function () {
+    return view('single-product-tab-style-right');
+});
+
+Route::get('/', function () {
+    return view('single-product-tab-style-top');
+});
+
+Route::get('/', function () {
+    return view('single-product');
+});
+
+Route::get('/', function () {
+    return view('wishlist');
+});
+
+Route::get('/', function () {
+    return view('single-product-tab-style-left');
+});
